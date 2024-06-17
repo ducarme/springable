0d4c926738dbac82947446ae191078fd63260d14
ducarme <paulducarme@hotmail.com>
2024-06-18 01:57:27 +0200

    update readme with google collab badge

--- src/springable/__init__.py
+++ src/springable/__init__.py
@@ -1,3 +1,16 @@
 __version__ = "0.0.3"
 __author__ = 'Paul Ducarme'
 __credits__ = 'AMOLF, ARCNL'
+
+print(f"""  ____             _                   _     _      
+ / ___| _ __  _ __(_)_ __   __ _  __ _| |__ | | ___ 
+ \___ \| '_ \| '__| | '_ \ / _` |/ _` | '_ \| |/ _ \\
+  ___) | |_) | |  | | | | | (_| | (_| | |_) | |  __/
+ |____/| .__/|_|  |_|_| |_|\__, |\__,_|_.__/|_|\___|
+       |_|                 |___/                    
+                
+Springable - Explore the richness of nonlinear mechanics with simple springs!
+Version {__version__}
+
+Paul Ducarme, AMOLF, Amsterdam (NL), 2024
+""")
