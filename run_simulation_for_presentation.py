ad196bbb40fbf7effaf5a2d52f58bb481a93fa1f
ducarme <paulducarme@hotmail.com>
2024-11-21 11:46:02 +0100

    new files for presentation

--- run_simulation_for_presentation.py
+++ run_simulation_for_presentation.py
@@ -1,7 +1,7 @@
-from src.springable.simulation import simulate_model
+from src.springable.simulation import simulate_model, scan_parameter_space
 from src.springable.visualization import make_animation
 from src.springable.readwrite import fileio
 
-simulate_model('models/presentation/simple_countersnapping.csv', 'results/presentation/simple_cs',
+simulate_model('models/presentation/pneunet_sensing.csv', 'results/presentation/pns',
                solver_settings_path='custom_solver_settings.toml',
                graphics_settings_path='custom_graphics_settings.toml')
