26f715a97c7163b5c46a06546930ad1bc81e830e
ducarme <paulducarme@hotmail.com>
2025-10-07 19:43:32 +0200

    final commit before version 1.0.0

--- src/springable/__init__.py
+++ src/springable/__init__.py
@@ -13,11 +13,12 @@ springable -- explore the richness of nonlinear mechanics with simple springs!
 
 Code: https://github.com/ducarme/springable
 Documentation: https://ducarme.github.io/springable/
+Issues: https://github.com/ducarme/springable/issues
 Releases: https://pypi.org/project/springable/
 
-Paul Ducarme, 2024-2025
+Paul Ducarme, 2021-2025
 """
 
-__version__ = "1.0.3"
+__version__ = "1.0.0"
 __author__ = 'Paul Ducarme (website: paulducarme.com, contact: paulducarme@hotmail.com)'
 __credits__ = 'AMOLF & ARCNL'
